536370c7ad6e92e6a6868d854636f2068c07aca4
Toma Rosa <toma.rosa@protonmail.com>
2023-10-08 14:29:55 +0200

    feat: intro animation

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "riot-red": "#eb0029",
+        "riot-white": "#f0f5f1",
+      },
+    },
+  },
+  plugins: [],
+};
